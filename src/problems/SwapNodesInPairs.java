87086a6405cd1f06c5e94eb32264fe22bda1f207
YMIR\syste <systemofadowng@gmail.com>
2022-12-26 16:17:45 -0500

    Swap Nodes In Pairs

---
+++ src/problems/SwapNodesInPairs.java
@@ -0,0 +1,40 @@
+package problems;
+
+import problems.listnode.ListNode;
+
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null) return head;
+        if(head.next == null) return head;
+
+        ListNode left = head;
+        ListNode right = head.next;
+
+        ListNode toReturn = right;
+
+        while(left != null && right != null) {
+            ListNode nextLeft = right.next;
+            if(nextLeft == null) { // next left is not present
+                right.next = left;
+                left.next = null;
+                break;
+            }
+            ListNode nextRight = right.next.next;
+            if(nextRight == null) { // next left is present, but right is not
+
+                right.next = left;
+                left.next = nextLeft;
+                break;
+            }
+            // both next are present
+
+            right.next = left;
+            left.next = nextRight;
+
+            left = nextLeft;
+            right = nextRight;
+        }
+
+        return toReturn;
+    }
+}
