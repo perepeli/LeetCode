f801514a14d609ca69c5561b39f311fa58749f15
YMIR\syste <systemofadowng@gmail.com>
2022-12-28 18:07:50 -0500

    Maximum Subarray

---
+++ src/problems/MaximumSubarray.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int maxSubarray = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int currentSubarray = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSubarray += nums[j];
+                maxSubarray = Math.max(maxSubarray, currentSubarray);
+            }
+        }
+
+        return maxSubarray;
+    }
+}
