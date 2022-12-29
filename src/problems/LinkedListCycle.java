98014f27ff86c11e62eeed39ca50ba30482bb2f3
YMIR\syste <systemofadowng@gmail.com>
2022-12-29 18:00:44 -0500

    Linked List Cycle

--- src/problems/LinkedListCycle.java
+++ src/problems/LinkedListCycle.java
@@ -1,6 +1,8 @@
+package problems;
+
 import problems.listnode.ListNode;
 
-public class SingleNumber {
+public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null) return false;
         ListNode slow = head;
