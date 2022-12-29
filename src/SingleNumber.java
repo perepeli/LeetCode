b8ed932eff4b2cf7279fdd8b842ed9b21d683b6e
YMIR\syste <systemofadowng@gmail.com>
2022-12-29 17:58:31 -0500

    Single Number

---
+++ src/SingleNumber.java
@@ -0,0 +1,21 @@
+import problems.listnode.ListNode;
+
+public class SingleNumber {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        int counter = 0;
+
+        while(fast != null) {
+            if(fast == slow) return true;
+            if(counter % 2 == 0) {
+                slow = slow.next;
+            }
+            fast = fast.next;
+            counter++;
+        }
+        return false;
+    }
+}
