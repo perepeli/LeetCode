6154c822f8e8f5fe2ff61be695520381d5e20e45
YMIR\syste <systemofadowng@gmail.com>
2023-01-03 20:18:48 -0500

    Number Of Good Pairs

---
+++ src/problems/NumberOfGoodPairs.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class NumberOfGoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int pairs = 0;
+        for(int i = 0; i < nums.length-1; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) pairs++;
+            }
+        }
+        return pairs;
+    }
+}
