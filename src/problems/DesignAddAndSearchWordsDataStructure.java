4a75263f60478b59a5cd29529268f8f3e2612323
YMIR\syste <systemofadowng@gmail.com>
2023-01-22 20:52:17 -0500

    Design Add And Search Words Data Structure

---
+++ src/problems/DesignAddAndSearchWordsDataStructure.java
@@ -0,0 +1,54 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean word = false;
+    public TrieNode() {}
+}
+public class DesignAddAndSearchWordsDataStructure {
+    TrieNode trie;
+
+    public DesignAddAndSearchWordsDataStructure() {
+        trie = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = trie;
+
+        for (char ch : word.toCharArray()) {
+            if (!node.children.containsKey(ch)) {
+                node.children.put(ch, new TrieNode());
+            }
+            node = node.children.get(ch);
+        }
+        node.word = true;
+    }
+
+    public boolean searchInNode(String word, TrieNode node) {
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+            if (!node.children.containsKey(ch)) {
+
+                if (ch == '.') {
+                    for (char x : node.children.keySet()) {
+                        TrieNode child = node.children.get(x);
+                        if (searchInNode(word.substring(i + 1), child)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            } else {
+                node = node.children.get(ch);
+            }
+        }
+        return node.word;
+    }
+
+    public boolean search(String word) {
+        return searchInNode(word, trie);
+    }
+}
