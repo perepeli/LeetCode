c91c4df917e1f2a084736b4f74373169d6efab72
YMIR\syste <systemofadowng@gmail.com>
2023-01-22 21:40:53 -0500

    Minimum Depth Of Binary Tree

---
+++ src/problems/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,18 @@
+package problems;
+
+import problems.listnode.TreeNode;
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        return recursive(root, 0);
+    }
+
+    private int recursive(TreeNode node, int depth) {
+        depth++;
+        if (node.left == null && node.right == null) return depth;
+        else if (node.left != null && node.right == null) return recursive(node.left, depth);
+        else if (node.right != null && node.left == null) return recursive(node.right, depth);
+        else return Math.min(recursive(node.left, depth), recursive(node.right, depth));
+    }
+}
