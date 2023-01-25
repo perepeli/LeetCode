7175428da55e20160bd485dd0e385bd3228e769f
YMIR\syste <systemofadowng@gmail.com>
2023-01-25 02:32:14 -0500

    Number Of Pairs

---
+++ src/problems/NumberOfPairs.java
@@ -0,0 +1,25 @@
+package problems;
+
+public class NumberOfPairs {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if(original.length != (m * n)) return new int[0][0];
+
+        int[][] arr = new int[m][n];
+
+        int row = 0;
+        int col = 0;
+
+        for(int i = 0; i < original.length; i++) {
+            arr[row][col] = original[i];
+
+            if(col == (n-1)) {
+                col = 0;
+                row++;
+            } else {
+                col++;
+            }
+        }
+
+        return arr;
+    }
+}
