2d0e089293a1b4827607b49a1bae6a2ea36467ae
YMIR\syste <systemofadowng@gmail.com>
2023-01-28 20:50:41 -0500

    Diameter Of Binary Tree

--- src/problems/DiameterOfBinaryTree.java
+++ src/problems/DiameterOfBinaryTree.java
@@ -3,19 +3,18 @@ package problems;
 import problems.listnode.TreeNode;
 
 public class DiameterOfBinaryTree {
-    public static int max = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        max = 0;
-        recursive(root, 0);
-        return max;
+        int[] out = {0};
+        recursive(root, 0, out);
+        return out[0];
     }
 
-    private int recursive(TreeNode node, int height) {
+    private int recursive(TreeNode node, int height, int[] max) {
         if(node == null) return 0;
 
-        int left = recursive(node.left, height);
-        int right = recursive(node.right, height);
-        max = Math.max(max, left + right);
+        int left = recursive(node.left, height, max);
+        int right = recursive(node.right, height, max);
+        max[0] = Math.max(max[0], left + right);
         return 1 + Math.max(left, right);
     }
 }
