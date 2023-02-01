e3b0381910910331df38c4d130e27311a4b07597
YMIR\syste <systemofadowng@gmail.com>
2023-01-31 20:55:01 -0500

    Permutations

--- src/problems/Permutations.java
+++ src/problems/Permutations.java
@@ -8,22 +8,22 @@ import java.util.Set;
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        recursive(nums, nums.length, new LinkedHashSet<>(), result);
+        recursive(nums, nums.length, new ArrayList<>(), result);
         return result;
     }
 
-    private void recursive(int[] arr, int depth, Set<Integer> excludeSet, List<List<Integer>> result) {
+    private void recursive(int[] arr, int depth, List<Integer> permutation, List<List<Integer>> result) {
         if(depth == 0) {
-            result.add(excludeSet.stream().toList());
+            result.add(permutation);
             return;
         }
 
         for(int i: arr) {
-            if(!excludeSet.contains(i)) {
-                Set<Integer> newSet = new LinkedHashSet<>(excludeSet);
-                newSet.add(i);
+            if(!permutation.contains(i)) {
+                List<Integer> newList = new ArrayList<>(permutation);
+                newList.add(i);
 
-                recursive(arr, depth - 1, newSet, result);
+                recursive(arr, depth - 1, newList, result);
             }
         }
     }
