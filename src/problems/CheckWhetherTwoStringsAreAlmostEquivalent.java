5ec4d1a480ffd93f1ec68a0b56f67089b6413d55
YMIR\syste <systemofadowng@gmail.com>
2023-02-08 22:33:27 -0500

    Check Whether Two Strings are Almost Equivalent

---
+++ src/problems/CheckWhetherTwoStringsAreAlmostEquivalent.java
@@ -0,0 +1,18 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckWhetherTwoStringsAreAlmostEquivalent {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < word1.length(); i++) {
+            map.put(word1.charAt(i), map.getOrDefault(word1.charAt(i), 0) + 1);
+            map.put(word2.charAt(i), map.getOrDefault(word2.charAt(i), 0) - 1);
+        }
+        for(int i: map.values()) {
+            if(i > 3 || i < -3) return false;
+        }
+        return true;
+    }
+}
