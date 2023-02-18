9de1ca57044bf8959d70bd41accec7e5c4a6ce59
YMIR\syste <systemofadowng@gmail.com>
2023-02-18 14:27:58 -0500

    Binary Tree Inorder Traversal

---
+++ src/problems/BinaryTreeInorderTraversal.java
@@ -0,0 +1,22 @@
+package problems;
+
+import problems.listnode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        recursive(root, result);
+        return result;
+    }
+
+    private void recursive(TreeNode node, List<Integer> result) {
+        if(node == null) return;
+
+        recursive(node.left, result);
+        result.add(node.val);
+        recursive(node.right, result);
+    }
+}
