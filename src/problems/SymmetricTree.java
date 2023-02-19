1c789117bc43b427cecee5df514570e2f71d46d3
YMIR\syste <systemofadowng@gmail.com>
2023-02-18 23:01:18 -0500

    Symmetric Tree

---
+++ src/problems/SymmetricTree.java
@@ -0,0 +1,21 @@
+package problems;
+
+import problems.listnode.TreeNode;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return false;
+        return recursive(root.left, root.right);
+    }
+
+    private boolean recursive(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left != null && right == null) return false;
+        if(right != null && left == null) return false;
+
+        if(left.val != right.val) return false;
+
+        return recursive(left.left, right.right) && recursive(left.right, right.left);
+
+    }
+}
