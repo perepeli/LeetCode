7be8fdb5c8bef9f5624e066ca19bbc5e12d0ac88
YMIR\syste <systemofadowng@gmail.com>
2023-02-19 15:39:18 -0500

    Intersection Of Two Linked Lists

---
+++ src/problems/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,24 @@
+package problems;
+
+import problems.listnode.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+
+        while(headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+
+        while(headB != null) {
+            if(set.contains(headB)) return headB;
+            headB = headB.next;
+        }
+
+        return null;
+    }
+}
