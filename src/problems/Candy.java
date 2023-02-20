91d059e6a16d2c192fe39c0229e85286ca9b2b5e
YMIR\syste <systemofadowng@gmail.com>
2023-02-20 13:09:37 -0500

    Candy

--- src/problems/Candy.java
+++ src/problems/Candy.java
@@ -4,29 +4,29 @@ import java.util.Arrays;
 
 public class Candy {
     public int candy(int[] ratings) {
-        int sum = 0;
-        int[] left = new int[ratings.length];
-        int[] right = new int[ratings.length];
-
-        Arrays.fill(left, 1);
-        Arrays.fill(right, 1);
+        int[] leftToRight = new int[ratings.length];
+        int[] rightToLeft = new int[ratings.length];
+        Arrays.fill(leftToRight, 1);
+        Arrays.fill(rightToLeft, 1);
 
         for(int i = 1; i < ratings.length; i++) {
             if(ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
+                leftToRight[i] = leftToRight[i - 1] + 1;
             }
         }
 
         for(int i = ratings.length - 2; i >= 0; i--) {
             if(ratings[i] > ratings[i + 1]) {
-                right[i] = right[i + 1] + 1;
+                rightToLeft[i] = rightToLeft[i + 1] + 1;
             }
         }
 
+        int result = 0;
+
         for(int i = 0; i < ratings.length; i++) {
-            sum += Math.max(left[i], right[i]);
+            result += Math.max(leftToRight[i], rightToLeft[i]);
         }
 
-        return sum;
+        return result;
     }
 }
