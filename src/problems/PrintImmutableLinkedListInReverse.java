a3b70e1230f5887325722e8c74bd4b5190667634
YMIR\syste <systemofadowng@gmail.com>
2023-02-25 14:40:59 -0500

    Print Immutable Linked List in Reverse

---
+++ src/problems/PrintImmutableLinkedListInReverse.java
@@ -0,0 +1,21 @@
+package problems;
+
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * interface ImmutableListNode {
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
+ * };
+ */
+
+public class PrintImmutableLinkedListInReverse {
+//    public void printLinkedListInReverse(ImmutableListNode head) { // ImmutableListNode implementation is hidden
+//        if(head == null) return;
+//
+//        printLinkedListInReverse(head.getNext());
+//        head.printValue();
+//    }
+}
+
+
