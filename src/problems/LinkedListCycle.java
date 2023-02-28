d0cf36c889d2f7e6f774fa481afcedec4614ba34
YMIR\syste <systemofadowng@gmail.com>
2023-02-27 22:20:37 -0500

    Linked List Cycle

--- src/problems/LinkedListCycle.java
+++ src/problems/LinkedListCycle.java
@@ -4,19 +4,15 @@ import problems.listnode.ListNode;
 
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
+
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
 
-        int counter = 0;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
 
-        while(fast != null) {
-            if(fast == slow) return true;
-            if(counter % 2 == 0) {
-                slow = slow.next;
-            }
-            fast = fast.next;
-            counter++;
+            if(slow == fast) return true;
         }
         return false;
     }
