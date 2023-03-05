1e2db4c52600fee5af9627913db7736974480a6f
YMIR\syste <systemofadowng@gmail.com>
2023-03-04 21:58:14 -0500

    Intersection of Three Sorted Arrays

---
+++ src/problems/IntersectionOfThreeSortedArrays.java
@@ -0,0 +1,32 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionOfThreeSortedArrays {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> res = new ArrayList<>();
+
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+
+        while(p1 < arr1.length && p2 < arr2.length && p3 < arr3.length) {
+            if(arr1[p1] == arr2[p2] && arr2[p2] == arr3[p3]) {
+                res.add(arr1[p1]);
+                p1++;
+                p2++;
+                p3++;
+            } else {
+                if(arr1[p1] < arr2[p2]) {
+                    p1++;
+                } else if(arr2[p2] < arr3[p3]) {
+                    p2++;
+                } else {
+                    p3++;
+                }
+            }
+        }
+        return res;
+    }
+}
