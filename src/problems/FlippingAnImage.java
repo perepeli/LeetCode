aba2706a13311e09d0409faf207dacb56c9b5212
YMIR\syste <systemofadowng@gmail.com>
2023-03-12 13:38:09 -0400

    Flipping an Image

---
+++ src/problems/FlippingAnImage.java
@@ -0,0 +1,22 @@
+package problems;
+
+public class FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] image) {
+
+        for(int i = 0; i < image.length; i++) {
+            for(int j = 0; j < image[i].length / 2; j++) {
+                int temp = image[i][image[i].length - j - 1];
+                image[i][image[i].length - j - 1] = image[i][j];
+                image[i][j] = temp;
+            }
+        }
+
+        for(int i = 0; i < image.length; i++) {
+            for(int j = 0; j < image[i].length; j++) {
+                image[i][j] = (image[i][j] == 0 ? 1 : 0);
+            }
+        }
+
+        return image;
+    }
+}
