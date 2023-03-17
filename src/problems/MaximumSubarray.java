ad9c41cd1bc8f05163e94a1eef114c1f3eb7b5c5
YMIR\syste <systemofadowng@gmail.com>
2023-03-17 18:56:11 -0400

    Maximum Subarray

--- src/problems/MaximumSubarray.java
+++ src/problems/MaximumSubarray.java
@@ -2,13 +2,13 @@ package problems;
 
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int max = nums[0];
+        int maxSum = nums[0];
+        int tempSum = nums[0];
 
         for(int i = 1; i < nums.length; i++) {
-            nums[i] = Math.max(nums[i], nums[i-1] + nums[i]);
-            max = Math.max(max, nums[i]);
+            tempSum = Math.max(tempSum + nums[i], nums[i]);
+            maxSum = Math.max(tempSum, maxSum);
         }
-
-        return max;
+        return maxSum;
     }
 }
