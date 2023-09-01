78ff228d2bd94c441afbc80d51224b48eda599db
YMIR\syste <systemofadowng@gmail.com>
2023-09-01 00:12:49 -0400

    Longest Increasing Subsequence

--- src/problems/LongestIncreasingSubsequence.java
+++ src/problems/LongestIncreasingSubsequence.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 public class LongestIncreasingSubsequence {
     public int lengthOfLIS(int[] nums) {
+        int res = 1;
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[j] < nums[i]) {
+        for(int i = nums.length-2; i >= 0; i--) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[j] > nums[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(res, dp[i]);
                 }
             }
         }
 
-        int max = 1;
-        for(int i : dp) max = Math.max(max, i);
-        return max;
+        return res;
     }
 }
