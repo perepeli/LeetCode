06ddc43576bb2dbf34a1a3b08b3a2d2b475ab196
YMIR\syste <systemofadowng@gmail.com>
2023-09-30 14:53:36 -0400

    Merge Two Sorted Lists

--- src/problems/MergeTwoSortedLists.java
+++ src/problems/MergeTwoSortedLists.java
@@ -4,40 +4,37 @@ package problems;
 import problems.util.ListNode;
 
 public class MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode rootToReturn = new ListNode();
-        ListNode tempNode = rootToReturn;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
 
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
-                tempNode.next = l1;
-                l1 = l1.next;
-
+        while(list1 != null && list2 != null) {
+            ListNode curr = new ListNode();
+            if(list1.val < list2.val) {
+                curr.val = list1.val;
+                list1 = list1.next;
             } else {
-                tempNode.next = l2;
-                l2 = l2.next;
+                curr.val = list2.val;
+                list2 = list2.next;
             }
-            tempNode = tempNode.next;
-
-
+            temp.next = curr;
+            temp = curr;
         }
 
-        if(l1 != null) {
-            while (l1 != null) {
-                tempNode.next = l1;
-                l1 = l1.next;
-                tempNode = tempNode.next;
-            }
+        while(list1 != null) {
+            ListNode curr = new ListNode(list1.val);
+            list1 = list1.next;
+            temp.next = curr;
+            temp = curr;
         }
 
-        if(l2 != null) {
-            while (l2 != null) {
-                tempNode.next = l2;
-                l2 = l2.next;
-                tempNode = tempNode.next;
-            }
+        while(list2 != null) {
+            ListNode curr = new ListNode(list2.val);
+            list2 = list2.next;
+            temp.next = curr;
+            temp = curr;
         }
 
-        return rootToReturn.next;
+        return dummy.next;
     }
 }
