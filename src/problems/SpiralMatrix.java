9bae7273ed5f7f90fd958bcaca220c2e0b1401b0
YMIR\syste <systemofadowng@gmail.com>
2023-10-21 20:37:40 -0400

    Spiral Matrix

--- src/problems/SpiralMatrix.java
+++ src/problems/SpiralMatrix.java
@@ -21,34 +21,26 @@ public class SpiralMatrix {
                     res.add(matrix[top][i]);
                 }
                 top++;
-                direction = (direction + 1) % 4;
-                continue;
             }
             if(direction == 1) {
                 for(int i = top; i <= bottom; i++) { // top to bottom
                     res.add(matrix[i][right]);
                 }
                 right--;
-                direction = (direction + 1) % 4;
-                continue;
             }
             if(direction == 2) {
                 for(int i = right; i >= left; i--) { // right to left
                     res.add(matrix[bottom][i]);
                 }
                 bottom--;
-                direction = (direction + 1) % 4;
-                continue;
             }
             if(direction == 3) {
                 for(int i = bottom; i >= top; i--) { // bottom to top
                     res.add(matrix[i][left]);
                 }
                 left++;
-                direction = (direction + 1) % 4;
-                continue;
             }
-
+            direction = (direction + 1) % 4;
         }
 
         return res;
