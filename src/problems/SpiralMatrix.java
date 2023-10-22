7a57a157920c7fc1897d847dfd21d0a5f6186ff9
YMIR\syste <systemofadowng@gmail.com>
2023-10-21 20:36:46 -0400

    Spiral Matrix

--- src/problems/SpiralMatrix.java
+++ src/problems/SpiralMatrix.java
@@ -1,66 +1,56 @@
 package problems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class SpiralMatrix {
-    public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
-
-        populateMatrix(matrix, n);
-
-        for(int i = 0; i < matrix.length; i++) {
-            System.out.println(Arrays.toString(matrix[i]));
-        }
-
-        return matrix;
-    }
-
-    private void populateMatrix(int[][] matrix, int n) {
-
+    public List<Integer> spiralOrder(int[][] matrix) {
         int top = 0;
-        int bottom = matrix.length-1;
+        int bottom = matrix.length - 1;
         int left = 0;
-        int right = matrix[0].length-1;
+        int right = matrix[0].length - 1;
 
+        List<Integer> res = new ArrayList<>();
 
-        int direction = 0; // 0 -> left, 1 -> down, 2 -> left, 3 -> up
-        int currentNumber = 1;
+        int direction = 0; // 0:=right, 1:=borttom, 2:=left, 3:=top
 
         while(left <= right && top <= bottom) {
-            if(direction == 0) { // from left to right
-                for(int i = left; i <= right; i++) {
-                    matrix[top][i] = currentNumber;
-                    currentNumber++;
+            if(direction == 0) {
+                for(int i = left; i <= right; i++) { // left to right
+                    res.add(matrix[top][i]);
                 }
                 top++;
+                direction = (direction + 1) % 4;
+                continue;
             }
-
-            if(direction == 1) { // from top to bottom
-
-                for(int i = top; i <= bottom; i++) {
-                    matrix[i][right] = currentNumber;
-                    currentNumber++;
+            if(direction == 1) {
+                for(int i = top; i <= bottom; i++) { // top to bottom
+                    res.add(matrix[i][right]);
                 }
                 right--;
+                direction = (direction + 1) % 4;
+                continue;
             }
-
-            if(direction == 2) { // from right to left
-                for(int i = right; i >= left; i--) {
-                    matrix[bottom][i] = currentNumber;
-                    currentNumber++;
+            if(direction == 2) {
+                for(int i = right; i >= left; i--) { // right to left
+                    res.add(matrix[bottom][i]);
                 }
                 bottom--;
+                direction = (direction + 1) % 4;
+                continue;
             }
-
-            if(direction == 3) { // from bottom to top
-                for(int i = bottom; i >= top; i--) {
-                    matrix[i][left] = currentNumber;
-                    currentNumber++;
+            if(direction == 3) {
+                for(int i = bottom; i >= top; i--) { // bottom to top
+                    res.add(matrix[i][left]);
                 }
                 left++;
+                direction = (direction + 1) % 4;
+                continue;
             }
 
-            direction = (direction + 1) % 4;
         }
+
+        return res;
     }
 }
