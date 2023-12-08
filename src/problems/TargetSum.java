9dd4722e0a2e6db98876b35f1e750da3ff89fb3b
YMIR\syste <systemofadowng@gmail.com>
2023-12-08 00:26:52 -0500

    Target Sum

---
+++ src/problems/TargetSum.java
@@ -0,0 +1,21 @@
+package problems;
+
+public class TargetSum {
+    public int findTargetSumWays(int[] nums, int target) {
+        int[] res = new int[]{0};
+        backtrack(nums, 0, target, 0, res);
+        return res[0];
+    }
+
+    private void backtrack(int[] nums, int index, int target, int sum, int[] res) {
+        if(index > nums.length) {
+            return;
+        } else if (index == nums.length) {
+            if(sum == target) res[0] += 1;
+        } else {
+            backtrack(nums, index + 1, target, sum + nums[index], res);
+
+            backtrack(nums, index + 1, target, sum - nums[index], res);
+        }
+    }
+}
