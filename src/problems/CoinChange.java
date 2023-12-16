7e1f91bb2c8091cd0a0cd357a2cb7d5136bacd9e
YMIR\syste <systemofadowng@gmail.com>
2023-12-15 20:52:19 -0500

    Coin Change

--- src/problems/CoinChange.java
+++ src/problems/CoinChange.java
@@ -1,26 +1,46 @@
 package problems;
 
+import java.util.Arrays;
+
 public class CoinChange {
-    private Integer[] dp;
+    private Integer[] memo;
 
     public int coinChange(int[] coins, int amount) {
-        dp = new Integer[amount + 1];
-        return recursive(coins, amount);
+        //memo = new Integer[amount + 1];
+        //return recursiveHelper(coins, amount);
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i = 1; i <  dp.length; i++) {
+            for(int j : coins) {
+                int prev = i - j;
+                if(prev < 0) continue;
+                if(dp[prev] == Integer.MAX_VALUE) continue;
+                dp[i] = Math.min(dp[i], 1 + dp[prev]);
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
 
-    private int recursive(int[] coins, int remain) {
+
+    private int recursiveHelper(int[] coins, int remain) {
         if(remain < 0) return -1;
         if(remain == 0) return 0;
 
-        if(dp[remain] != null) return dp[remain];
+        if(memo[remain] != null) return memo[remain];
 
         int minCount = Integer.MAX_VALUE;
+
         for(int coin : coins) {
-            int count = recursive(coins, remain - coin);
+            int count = recursiveHelper(coins, remain - coin);
             if(count == -1) continue;
             minCount = Math.min(minCount, count + 1);
         }
-        dp[remain] = minCount == Integer.MAX_VALUE ? -1 : minCount;
-        return dp[remain];
+
+        memo[remain] = minCount == Integer.MAX_VALUE ? -1 : minCount;
+        return memo[remain];
     }
 }
