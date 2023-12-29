66e79886d4412142a9821ee3aaf9e424a2a17a68
YMIR\syste <systemofadowng@gmail.com>
2023-12-29 02:09:01 -0500

    Word Search

--- src/problems/WordSearch.java
+++ src/problems/WordSearch.java
@@ -3,54 +3,42 @@ package problems;
 import java.util.Arrays;
 
 public class WordSearch {
-    static final int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    tatic final int[][] DIRECTIONS = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public boolean exist(char[][] board, String word) {
-        int row = board.length;
-        int col = board[0].length;
+        char[] wordArray = word.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        boolean[] res = new boolean[]{false};
+        int r = board.length;
+        int c = board[0].length;
 
-        boolean[][] visited = new boolean[row][col];
-
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(backtrack(board, word, 0, i, j, visited)) return true;
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                backtrack(board, wordArray, sb, i, j, res);
+                if(res[0] == true) return true;
             }
-        }
-        for(boolean[] i : visited) {
-            System.out.println(Arrays.toString(i));
         }
 
         return false;
     }
 
-    private boolean backtrack(char[][] board, String word, int index, int i, int j, boolean[][] visited) {
-        if(index == word.length()) {
-            return true;
-        }
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
-            return false;
-        }
-        if(visited[i][j]) {
-            return false;
-        }
-        if(board[i][j] == word.charAt(index)) {
-            boolean found = false;
+    private void backtrack(char[][] board, char[] word, StringBuilder sb, int r, int c, boolean[] res) {
+        if(sb.length() == word.length) {
+            res[0] = true;
+        } else {
+            if(r < 0 || c < 0 || r >= board.length || c >= board[0].length) return;
+            if(board[r][c] == '*') return;
+            if(board[r][c] == word[sb.length()]) {
+                char prevChar = board[r][c];
+                board[r][c] = '*';
+                sb.append(board[r][c]);
+                for(int[] dir : DIRECTIONS) {
+                    backtrack(board, word, sb, r + dir[0], c + dir[1], res);
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                board[r][c] = prevChar;
 
-            for(int[] direction : directions) {
-                visited[i][j] = true;
-
-                int R = direction[0];
-                int C = direction[1];
-
-                found = backtrack(board, word, index + 1, i + R, j + C, visited);
-
-                visited[i][j] = false;
-
-                if(found) break;
             }
-
-            return found;
         }
-        return false;
     }
 }
