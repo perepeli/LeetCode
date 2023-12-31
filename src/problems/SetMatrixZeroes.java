f8e415f6e5b565bd79c3b605f48f32b4c11557e7
YMIR\syste <systemofadowng@gmail.com>
2023-12-31 02:46:43 -0500

    Set Matrix Zeroes

--- src/problems/SetMatrixZeroes.java
+++ src/problems/SetMatrixZeroes.java
@@ -2,48 +2,26 @@ package problems;
 
 public class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] columns = new boolean[matrix[0].length];
 
         for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j] == 0 && visited[i][j] == false) {
-                    visitAllTop(matrix, i,j, visited);
-                    visitAllBottom(matrix, i,j, visited);
-                    visitAllLeft(matrix, i,j, visited);
-                    visitAllRight(matrix, i,j, visited);
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    rows[i] = true;
+                    columns[j] = true;
                 }
             }
         }
 
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(rows[i] == true || columns[j] == true) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
 
-
-        // for(int i = 0; i < matrix.length; i++) {
-        //     System.out.println(Arrays.toString(matrix[i]));
-        // }
-    }
-
-    private void visitAllLeft(int[][] arr, int row, int col, boolean[][] visited) {
-        if(col < 0) return;
-        if(arr[row][col] != 0) visited[row][col] = true;
-        arr[row][col] = 0;
-        visitAllLeft(arr, row, col-1, visited);
-    }
-    private void visitAllRight(int[][] arr, int row, int col, boolean[][] visited) {
-        if(col > arr[row].length-1) return;
-        if(arr[row][col] != 0) visited[row][col] = true;
-        arr[row][col] = 0;
-        visitAllRight(arr, row, col+1, visited);
-    }
-    private void visitAllTop(int[][] arr, int row, int col, boolean[][] visited) {
-        if(row < 0) return;
-        if(arr[row][col] != 0) visited[row][col] = true;
-        arr[row][col] = 0;
-        visitAllTop(arr, row-1, col, visited);
-    }
-    private void visitAllBottom(int[][] arr, int row, int col, boolean[][] visited) {
-        if(row > arr.length-1) return;
-        if(arr[row][col] != 0) visited[row][col] = true;
-        arr[row][col] = 0;
-        visitAllBottom(arr, row+1, col, visited);
     }
 }
