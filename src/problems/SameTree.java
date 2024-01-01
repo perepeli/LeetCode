4be339c10eee1421c1694a8affbc4dc5906ca3eb
YMIR\syste <systemofadowng@gmail.com>
2024-01-01 02:37:25 -0500

    Same Tree

--- src/problems/SameTree.java
+++ src/problems/SameTree.java
@@ -5,7 +5,7 @@ import problems.util.TreeNode;
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if((p == null && q != null) || (p != null && q == null) || p.val != q.val) return false;
+        if((p == null || q == null) || p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
