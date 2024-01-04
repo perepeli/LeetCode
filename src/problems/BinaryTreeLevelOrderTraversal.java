a753f138b16cc2e65a17092e326e891a80463e89
YMIR\syste <systemofadowng@gmail.com>
2024-01-04 02:46:51 -0500

    Binary Tree Level Order Traversal

--- src/problems/BinaryTreeLevelOrderTraversal.java
+++ src/problems/BinaryTreeLevelOrderTraversal.java
@@ -5,47 +5,29 @@ import problems.util.TreeNode;
 import java.util.*;
 
 public class BinaryTreeLevelOrderTraversal {
-    public List<List<Integer>> levelOrder1(TreeNode root) { // dfs solution
-        List<List<Integer>> levels = new ArrayList<>();
-        if (root == null) return levels;
-        recursive(root, 1, levels);
-        return levels;
-    }
-
-    private void recursive(TreeNode node, int level, List<List<Integer>> levels) {
-        if(levels.size() < level) levels.add(new ArrayList<>());
-        levels.get(level-1).add(node.val);
-
-
-        if(node.left != null) recursive(node.left, level + 1, levels);
-        if(node.right != null) recursive(node.right, level + 1, levels);
-    }
-
-    public List<List<Integer>> levelOrder2(TreeNode root) { // bfs iterative
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
         Map<Integer, List<Integer>> map = new HashMap<>();
 
-        Queue<AbstractMap.SimpleEntry<Integer, TreeNode>> queue = new LinkedList<>();
-        queue.offer(new AbstractMap.SimpleEntry<>(1, root));
+        dfs(root, 1, map);
 
-        while(!queue.isEmpty()) {
-            var pair = queue.poll();
-            int key = pair.getKey();
-            TreeNode node = pair.getValue();
+        int tempLevel = 1;
 
-            if(node == null) continue;
-
-            if(map.containsKey(key)) {
-                map.get(key).add(node.val);
-            } else {
-                List<Integer> newList = new ArrayList<>();
-                newList.add(node.val);
-                map.put(key, newList);
-            }
-
-            queue.add(new AbstractMap.SimpleEntry<>(key + 1, node.left));
-            queue.add(new AbstractMap.SimpleEntry<>(key + 1, node.right));
+        while(map.containsKey(tempLevel)) {
+            res.add(map.get(tempLevel));
+            tempLevel++;
         }
 
-        return new ArrayList<>(map.values());
+        return res;
+    }
+
+    private void dfs(TreeNode node, int level, Map<Integer, List<Integer>> map) {
+        if(node == null) return;
+
+        if(!map.containsKey(level)) map.put(level, new ArrayList<>());
+        map.get(level).add(node.val);
+
+        dfs(node.left, level + 1, map);
+        dfs(node.right, level + 1, map);
     }
 }
