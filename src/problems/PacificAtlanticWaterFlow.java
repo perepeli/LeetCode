b0e3dc6d67f9f428dd8325e2394ef17b64a6504d
YMIR\syste <systemofadowng@gmail.com>
2024-01-03 23:44:03 -0500

    Pacific Atlantic Water Flow

--- src/problems/PacificAtlanticWaterFlow.java
+++ src/problems/PacificAtlanticWaterFlow.java
@@ -1,69 +1,43 @@
 package problems;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import problems.util.Pair;
+
+import java.util.*;
 
 public class PacificAtlanticWaterFlow {
-    int[][] directions = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
-    int[][] landHeights;
-    int R;
-    int C;
+    private static final int[][] directions = new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
 
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        if(heights.length == 0 || heights[0].length == 0) return new ArrayList<>();
+        Set<Pair<Integer, Integer>> pacific = new HashSet<>();
+        Set<Pair<Integer, Integer>> atlantic = new HashSet<>();
+
+        for (int i = 0; i < heights[0].length; i++) dfs(0, i, heights, heights[0][i], pacific);
+        for (int i = 0; i < heights.length; i++) dfs(i, 0, heights, heights[i][0], pacific);
+
+        for (int i = 0; i < heights[0].length; i++) dfs(heights.length - 1, i, heights, heights[heights.length - 1][i], atlantic);
+        for (int i = 0; i < heights.length; i++) dfs(i, heights[0].length - 1, heights, heights[i][heights[0].length - 1], atlantic);
 
         List<List<Integer>> res = new ArrayList<>();
 
-        landHeights = heights;
-        R = heights.length;
-        C = heights[0].length;
-
-        Queue<int[]> pacificQ = new LinkedList<>();
-        Queue<int[]> atlanticQ = new LinkedList<>();
-
-        for(int r = 0; r < R; r++) {
-            pacificQ.offer(new int[]{r,0});
-            atlanticQ.offer(new int[]{r,C-1});
-        }
-
-        for(int c = 0; c < C; c++) {
-            pacificQ.offer(new int[]{0,c});
-            atlanticQ.offer(new int[]{R-1,c});
-        }
-
-        boolean[][] pacific = bfs(pacificQ);
-        boolean[][] atlantic = bfs(atlanticQ);
-
-        for(int r = 0; r < R; r++) {
-            for(int c = 0; c < C; c++) {
-                if(pacific[r][c] && atlantic[r][c]) res.add(List.of(r, c));
+        for(Pair<Integer, Integer> p : pacific) {
+            if(atlantic.contains(p)) {
+                res.add(List.of(p.getKey(), p.getValue()));
             }
         }
 
         return res;
     }
 
-    private boolean[][] bfs(Queue<int[]> queue) {
-        boolean[][] reachable = new boolean[R][C];
+    private void dfs(int r, int c, int[][] arr, int fromHeight, Set<Pair<Integer, Integer>> set) {
+        if(r < 0 || c < 0 || r >= arr.length || c >= arr[0].length) return;
+        if(fromHeight > arr[r][c]) return;
 
-        while(!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            reachable[cell[0]][cell[1]] = true;
-            for(int[] dir : directions) {
-                int newRow = cell[0] + dir[0];
-                int newCol = cell[1] + dir[1];
+        Pair<Integer, Integer> p = new Pair<>(r, c);
+        if(set.contains(p)) return;
+        set.add(p);
 
-                if(newRow < 0 || newRow >= R || newCol < 0 || newCol >= C) continue;
-
-                if(reachable[newRow][newCol]) continue;
-
-                if(landHeights[newRow][newCol] < landHeights[cell[0]][cell[1]]) continue;
-                queue.offer(new int[]{newRow, newCol});
-            }
+        for(int[] dir : directions) {
+            dfs(r + dir[0], c + dir[1], arr, arr[r][c], set);
         }
-
-        return reachable;
     }
 }
