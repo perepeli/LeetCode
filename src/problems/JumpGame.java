7c2b350446041a4a6bae7e5d1224e01e2ff7f79c
YMIR\syste <systemofadowng@gmail.com>
2024-01-05 01:49:11 -0500

    Jump Game

--- src/problems/JumpGame.java
+++ src/problems/JumpGame.java
@@ -2,21 +2,20 @@ package problems;
 
 public class JumpGame {
     public boolean canJump(int[] nums) {
-        int len = nums.length;
+        boolean[] dp = new boolean[nums.length];
 
-        boolean[] dp = new boolean[len];
-        dp[len-1] = true;
-
-        for(int i = len-1; i >= 0; i--) {
-            int counter = nums[i];
-            for(int j = i+1; j < len && counter > 0; j++) {
-                if(dp[j] == true) {
-                    dp[i] = true;
-                    break;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(i == nums.length - 1) dp[i] = true;
+            else {
+                for(int j = i; j <= i + nums[i]  && j < nums.length; j++) {
+                    if(dp[j] == true) {
+                        dp[i] = true;
+                        break;
+                    }
                 }
-                counter--;
             }
         }
+
         return dp[0];
     }
 }
