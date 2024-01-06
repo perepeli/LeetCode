c98ad86d46be1ae239c644e13e5ed41e7369d66d
YMIR\syste <systemofadowng@gmail.com>
2024-01-05 22:02:02 -0500

    Reverse Linked List II

---
+++ src/problems/ReverseLinkedList2.java
@@ -0,0 +1,41 @@
+package problems;
+
+public class ReverseLinkedList2 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left >= right) return head;
+        ListNode leftPrev = null;
+        ListNode leftNode = head;
+        int leftIndex = 1;
+
+        while(leftIndex != left) {
+            leftPrev = leftNode;
+            leftNode = leftNode.next;
+            leftIndex++;
+        }
+
+        int rightIndex = leftIndex;
+        ListNode rightNode = leftNode;
+        while(rightIndex != right) {
+            rightNode = rightNode.next;
+            rightIndex++;
+        }
+        ListNode rightNext = rightNode.next;
+
+        ListNode prev = leftPrev;
+        ListNode curr = leftNode;
+
+        while(curr != rightNext) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        leftNode.next = rightNext;
+        if(leftPrev == null) return prev;
+        leftPrev.next = prev;
+
+        return head;
+    }
+}
