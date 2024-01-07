efb607b8c9fe084c688b7a83b76e04697f3d17c5
YMIR\syste <systemofadowng@gmail.com>
2024-01-06 20:27:12 -0500

    Kth Largest Element in an Array

--- src/problems/KthLargestElementInAnArray.java
+++ src/problems/KthLargestElementInAnArray.java
@@ -4,12 +4,17 @@ import java.util.PriorityQueue;
 
 public class KthLargestElementInAnArray {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // min heap
 
         for(int i : nums) {
-            pq.add(i);
-
-            if(pq.size() > k) pq.poll();
+            if(pq.size() < k) {
+                pq.add(i);
+            } else {
+                if(pq.peek() < i) {
+                    pq.poll();
+                    pq.offer(i);
+                }
+            }
         }
 
         return pq.peek();
