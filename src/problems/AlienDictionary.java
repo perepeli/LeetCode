b604fa0e6ef0388ee59b793e6783727bc30c35bd
YMIR\syste <systemofadowng@gmail.com>
2024-01-12 22:35:27 -0500

    Alien Dictionary

--- src/problems/AlienDictionary.java
+++ src/problems/AlienDictionary.java
@@ -1,54 +1,85 @@
 package problems;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 import problems.util.Pair;
 
 public class AlienDictionary {
 
     public String alienOrder(String[] words) {
-        //if(words.length == 1) return words[0]; // corner case : aba -> invalid
-        //abbc
-        //abcd
+        Map<Character, Set<Character>> adjList = new HashMap<>(); // dependency to <set>dependant
+        Map<Character, Integer> inDegree = new HashMap<>(); // dependant -> inDegree
 
-        Deque<Pair<String, Integer>> queue = new ArrayDeque<>();
+        for(int i = 1; i < words.length; i++) {
+            String dependant = words[i - 1];
+            String dependency = words[i];
 
-        for(int i = 0; i < words.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            String s = words[i];
+            int j = 0;
 
-            for(int j = 0; j < s.length(); j++) {
-                if(j > 0 && s.charAt(j) == s.charAt(j - 1)) continue;
-                sb.append(s.charAt(j));
+            while(j < dependant.length() && j < dependency.length()) {
+                char dependantChar = dependant.charAt(j);
+                char dependencyChar = dependency.charAt(j);
+
+                if(dependantChar == dependencyChar && ((j == dependant.length() - 1 || j == dependency.length() - 1) && (dependant.length() > dependency.length()))) return "";
+
+                if(!adjList.containsKey(dependantChar)) adjList.put(dependantChar, new HashSet<>());
+                if(!adjList.containsKey(dependencyChar)) adjList.put(dependencyChar, new HashSet<>());
+
+                if(!inDegree.containsKey(dependantChar)) inDegree.put(dependantChar, 0);
+                if(!inDegree.containsKey(dependencyChar)) inDegree.put(dependencyChar, 0);
+
+                if(dependantChar != dependencyChar) {
+                    if(!adjList.get(dependencyChar).contains(dependantChar)) {
+                        adjList.get(dependencyChar).add(dependantChar);
+                        inDegree.put(dependantChar, inDegree.get(dependantChar) + 1);
+                    }
+
+                    break;
+                }
+
+                j++;
             }
-            queue.addLast(new Pair<>(sb.toString(), 0));
         }
 
+        Deque<Character> queue = new ArrayDeque<>();
+        for(Map.Entry<Character, Integer> e : inDegree.entrySet()) {
+            if(e.getValue() == 0) {
+                queue.offer(e.getKey());
+            }
+        }
 
-        char prev = '*';
+        StringBuilder sb = new StringBuilder();
 
         while(!queue.isEmpty()) {
-            Pair<String, Integer> p = queue.removeFirst();
-            String s = p.getKey();
-            int index = p.getValue();
-            if(index >= s.length()) continue;
+            Character dependency = queue.poll();
+            sb.append(dependency);
 
-            Deque<Pair<String, Integer>> tmpStack = new ArrayDeque<>();
-
-            while(!queue.isEmpty()) {
-                char ch = queue.peekFirst().getKey().charAt(queue.peekFirst().getValue());
-                if(queue.peekFirst().getKey().charAt(queue.peekFirst().getValue()) != s.charAt(index)) break;
-                Pair<String, Integer> tmp = queue.removeFirst();
-                tmpStack.addLast(new Pair<>(tmp.getKey(), tmp.getValue() + 1));
+            for(Character dependant : adjList.get(dependency)) {
+                inDegree.put(dependant, inDegree.get(dependant) - 1);
+                if(inDegree.get(dependant) == 0) {
+                    queue.offer(dependant);
+                }
             }
 
-            while (!tmpStack.isEmpty()) {
-                queue.addFirst(tmpStack.removeLast());
-            }
-            System.out.println(prev + " -> " + s.charAt(index));
-
-            prev = s.charAt(index);
         }
+
+        for(Map.Entry<Character, Integer> e : inDegree.entrySet()) {
+            if(e.getValue() != 0) {
+                return "";
+            }
+        }
+
+        Set<Character> allChars = new HashSet<>();
+        for(String w : words) {
+            for(int i = 0; i < w.length(); i++) allChars.add(w.charAt(i));
+        }
+
+        sb.reverse();
+
+        for(Character c : allChars) {
+            if(!adjList.containsKey(c)) sb.append(c);
+        }
+
+        return sb.toString();
     }
 }
