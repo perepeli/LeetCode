0c80d3745568947e8a359f54eae9a9743b955781
YMIR\syste <systemofadowng@gmail.com>
2024-05-02 23:23:22 -0400

    Merge Intervals

--- src/problems/MergeIntervals.java
+++ src/problems/MergeIntervals.java
@@ -4,34 +4,26 @@ import java.util.*;
 
 public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
-        Set<Integer> set = new LinkedHashSet<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((i1, i2) -> i1[0] - i2[0]);
+        for(int[] i : intervals) pq.offer(i);
 
-        for(int[] e : intervals) {
-            for(int i = e[0]; i <= e[1]; i++) {
-                set.add(i);
+        List<int[]> res = new ArrayList<>();
+
+        while(!pq.isEmpty()) {
+            int[] mergedInterval = pq.poll();
+            while(!pq.isEmpty() && pq.peek()[0] <= mergedInterval[1]) {
+                int[] interval = pq.poll();
+                mergedInterval[1] = Math.max(mergedInterval[1], interval[1]);
             }
+            res.add(mergedInterval);
         }
 
-        Integer[] arr = set.toArray(new Integer[0]);
-        System.out.println(Arrays.toString(arr));
-        int left = 0;
-        int right = 1;
+        int[][] resArray = new int[res.size()][2];
 
-        List<int[]> list = new ArrayList<>();
-
-        while(right < arr.length) {
-            if(arr[right-1] == arr[right]-1) {
-                if(right == arr.length-1) {
-                    list.add(new int[]{arr[left], arr[right]});
-                }
-                right++;
-            } else {
-                list.add(new int[]{arr[left], arr[right-1]});
-                left = right;
-                right = left+1;
-            }
+        for(int i = 0; i < res.size(); i++) {
+            resArray[i] = res.get(i);
         }
 
-        return list.toArray(new int[0][0]) ;
+        return resArray;
     }
 }
