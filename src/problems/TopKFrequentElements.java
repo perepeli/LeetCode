e9240f5459a134a50670deabc92803a5cfe94bd5
YMIR\syste <systemofadowng@gmail.com>
2024-05-04 18:11:41 -0400

    Top K Frequent Elements

--- src/problems/TopKFrequentElements.java
+++ src/problems/TopKFrequentElements.java
@@ -7,31 +7,30 @@ import java.util.Queue;
 
 public class TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> frequency = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
 
-        for(int i : nums) {
-            frequency.putIfAbsent(i, 0);
-            frequency.put(i, frequency.get(i) + 1);
-        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> p1[1] - p2[1]);
 
-        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
-        for(Map.Entry<Integer, Integer> e : frequency.entrySet()) {
-            if(pq.size() == k) {
-                if(pq.peek().getValue() < e.getValue()) {
-                    pq.poll();
-                    pq.offer(e);
-                }
+        for(var e : map.entrySet()) {
+            int[] elem = new int[]{e.getKey(), e.getValue()};
+
+            if(k > 0) {
+                pq.offer(elem);
+                k--;
             } else {
-                pq.offer(e);
+                if(pq.peek()[1] < elem[1]) {
+                    pq.poll();
+                    pq.offer(elem);
+                }
             }
         }
 
-        int[] res = new int[k];
-
-        while(k > 0) {
-            res[res.length - k] = pq.poll().getKey();
-            k--;
+        int[] res = new int[pq.size()];
+        int index = 0;
+        while(!pq.isEmpty()) {
+            res[index++] = pq.poll()[0];
         }
 
         return res;
