5f4ff3e23e7d460c4da0259ccc3f94d97994d42c
YMIR\syste <systemofadowng@gmail.com>
2024-05-04 16:46:59 -0400

    Longest Consecutive Sequence

--- src/problems/LongestConsecutiveSequence.java
+++ src/problems/LongestConsecutiveSequence.java
@@ -4,47 +4,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LongestConsecutiveSequence {
-    public int longestConsecutive(int[] nums) { // set O(n)
+    public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
 
-        for(int i : nums) set.add(i);
-
-        int longestStreak = 0;
+        int longest = 0;
 
         for(int i : set) {
             if(set.contains(i - 1)) continue;
-
-            int current = i;
-            int tempStreak = 1;
-
-            while(set.contains(current + 1)) {
-                current++;
-                tempStreak++;
+            int tempLength = 1;
+            int tempNum = i + 1;
+            while(set.contains(tempNum)) {
+                tempLength++;
+                tempNum++;
             }
-
-            longestStreak = Math.max(longestStreak, tempStreak);
+            longest = Math.max(longest, tempLength);
         }
 
-        return longestStreak;
+        return longest;
     }
-
-    // public int longestConsecutive(int[] nums) { // sorting O(nlogn)
-    //     if(nums.length == 0) return 0;
-    //     Arrays.sort(nums);
-
-    //     int longestSequence = 1;
-    //     int tempSequense = 1;
-
-    //     for(int i = 1; i < nums.length; i++) {
-    //         if(nums[i] == nums[i - 1]) continue;
-
-    //         if(nums[i] == nums[i - 1] + 1) {
-    //             tempSequense++;
-    //             longestSequence = Math.max(longestSequence, tempSequense);
-    //         } else {
-    //             tempSequense = 1;
-    //         }
-    //     }
-    //     return longestSequence;
-    // }
 }
