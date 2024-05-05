c80dc9a48a7ceeeaa1673153d409ae4e4b9e6480
YMIR\syste <systemofadowng@gmail.com>
2024-05-04 21:33:24 -0400

    Valid Sudoku

--- src/problems/ValidSudoku.java
+++ src/problems/ValidSudoku.java
@@ -1,88 +1,48 @@
 package problems;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
 
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                if(i == j) {
-                    if(!isRowValid(board, i, j)) return false;
-                    if(!isColumnValid(board, i, j)) return false;
-                }
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Map<Integer, Set<Character>>> quadrant = new HashMap<>();
 
-                if(((i+2) % 3 == 0) && ((j+2) % 3 == 0)) {
-                    if (!isSubgridValid(board, i, j)) return false;
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(board[row][col] != '.') {
+                    int quadrantRow = map(row);
+                    int quadrantCol = map(col);
+
+                    if(!rows.containsKey(row)) rows.put(row, new HashSet<>());
+                    if(!cols.containsKey(col)) cols.put(col, new HashSet<>());
+                    if(!quadrant.containsKey(quadrantRow)) quadrant.put(quadrantRow, new HashMap<>());
+                    if(!quadrant.get(quadrantRow).containsKey(quadrantCol)) quadrant.get(quadrantRow).put(quadrantCol, new HashSet<>());
+
+                    if(rows.get(row).contains(board[row][col])) return false;
+                    if(cols.get(col).contains(board[row][col])) return false;
+                    if(quadrant.get(quadrantRow).get(quadrantCol).contains(board[row][col])) return false;
+
+                    rows.get(row).add(board[row][col]);
+                    cols.get(col).add(board[row][col]);
+                    quadrant.get(quadrantRow).get(quadrantCol).add(board[row][col]);
                 }
             }
         }
+
         return true;
     }
 
-    private boolean isSubgridValid(char[][] arr, int row, int col) { // only call from the middle of a subgrid
-        Set<Character> set = new HashSet<>();
-        if(arr[row][col] != '.') { //this
-            if(!set.contains(arr[row][col])) set.add(arr[row][col]);
-            else return false;
-        }
-        if(arr[row-1][col-1] != '.') { // top-left
-            if(!set.contains(arr[row-1][col-1])) set.add(arr[row-1][col-1]);
-            else return false;
-        }
-        if(arr[row-1][col] != '.') { // top
-            if(!set.contains(arr[row-1][col])) set.add(arr[row-1][col]);
-            else return false;
-        }
-        if(arr[row-1][col+1] != '.') { // top-right
-            if(!set.contains(arr[row-1][col+1])) set.add(arr[row-1][col+1]);
-            else return false;
-        }
-        if(arr[row][col-1] != '.') { // left
-            if(!set.contains(arr[row][col-1])) set.add(arr[row][col-1]);
-            else return false;
-        }
-        if(arr[row][col+1] != '.') { // right
-            if(!set.contains(arr[row][col+1])) set.add(arr[row][col+1]);
-            else return false;
-        }
-        if(arr[row+1][col-1] != '.') { // bottom-left
-            if(!set.contains(arr[row+1][col-1])) set.add(arr[row+1][col-1]);
-            else return false;
-        }
-        if(arr[row+1][col] != '.') { // bottom
-            if(!set.contains(arr[row+1][col])) set.add(arr[row+1][col]);
-            else return false;
-        }
-        if(arr[row+1][col+1] != '.') { // bottom right
-            if(!set.contains(arr[row+1][col+1])) set.add(arr[row+1][col+1]);
-            else return false;
-        }
-
-        //System.out.println(set);
-        return true;
-    }
-
-    private boolean isRowValid(char[][] arr, int row, int col) { // no duplicates
-        Set<Character> set = new HashSet<>();
-
-        for(int i = 0; i < arr[row].length; i++) {
-            if(arr[row][i] == '.') continue;
-            if(!set.contains(arr[row][i])) set.add(arr[row][i]);
-            else return false;
-        }
-        return true;
-    }
-
-    private boolean isColumnValid(char[][] arr, int row, int col) { // no duplicates
-        Set<Character> set = new HashSet<>();
-
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i][col] == '.') continue;
-            if(!set.contains(arr[i][col])) set.add(arr[i][col]);
-            else return false;
-        }
-        return true;
+    private int map(int num) {
+        if(num < 3) return 0;
+        if(num < 6) return 1;
+        if(num < 9) return 2;
+        throw new IllegalArgumentException("Invalid input");
     }
 }
