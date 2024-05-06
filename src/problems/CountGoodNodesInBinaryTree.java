8c8590dd5d817f7e603adc71e4497607e7c421be
YMIR\syste <systemofadowng@gmail.com>
2024-05-06 02:41:33 -0400

    Count Good Nodes in Binary Tree

--- src/problems/CountGoodNodesInBinaryTree.java
+++ src/problems/CountGoodNodesInBinaryTree.java
@@ -5,17 +5,18 @@ import problems.util.TreeNode;
 public class CountGoodNodesInBinaryTree {
     public int goodNodes(TreeNode root) {
         int[] res = new int[1];
-        recursive(root, root.val, res);
+        dfs(root, root.val, res);
         return res[0];
-
     }
 
-    private void recursive(TreeNode node, int max, int[] res) {
+    private void dfs(TreeNode node, int max, int[] res) {
         if(node == null) return;
 
         if(node.val >= max) res[0]++;
 
-        recursive(node.left, Math.max(max, node.val), res);
-        recursive(node.right, Math.max(max, node.val), res);
+        max = Math.max(max, node.val);
+
+        dfs(node.left, max, res);
+        dfs(node.right, max, res);
     }
 }
