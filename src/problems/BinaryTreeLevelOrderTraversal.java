f7ee8a87451b5dce9c2393e499af2237a5eb3a7e
YMIR\syste <systemofadowng@gmail.com>
2024-05-06 02:22:58 -0400

    Binary Tree Level Order Traversal

--- src/problems/BinaryTreeLevelOrderTraversal.java
+++ src/problems/BinaryTreeLevelOrderTraversal.java
@@ -7,27 +7,16 @@ import java.util.*;
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
-        dfs(root, 1, map);
-
-        int tempLevel = 1;
-
-        while(map.containsKey(tempLevel)) {
-            res.add(map.get(tempLevel));
-            tempLevel++;
-        }
-
+        dfs(root, 1, res);
         return res;
     }
 
-    private void dfs(TreeNode node, int level, Map<Integer, List<Integer>> map) {
+    private void dfs(TreeNode node, int level, List<List<Integer>> res) {
         if(node == null) return;
+        if(res.size() < level) res.add(new ArrayList<>());
+        res.get(level - 1).add(node.val);
 
-        if(!map.containsKey(level)) map.put(level, new ArrayList<>());
-        map.get(level).add(node.val);
-
-        dfs(node.left, level + 1, map);
-        dfs(node.right, level + 1, map);
+        dfs(node.left, level + 1, res);
+        dfs(node.right, level + 1, res);
     }
 }
