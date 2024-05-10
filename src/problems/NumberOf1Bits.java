2f7fcd21cb9aad2d210efc849d8eb04cf5e14394
YMIR\syste <systemofadowng@gmail.com>
2024-05-09 23:43:14 -0400

    Number Of 1 Bits

--- src/problems/NumberOf1Bits.java
+++ src/problems/NumberOf1Bits.java
@@ -3,11 +3,12 @@ package problems;
 public class NumberOf1Bits {
     public int hammingWeight(int n) {
         int bits = 0;
-        int shift = 1;
-
-        for(int i = 32; i >= 1; i--) {
-            if(!((n & shift) == 0)) bits++;
-            shift += shift;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask <<= 1;
         }
         return bits;
     }
