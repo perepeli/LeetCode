86f5130294892f3e2a94e8ecf26949b08b30cf70
YMIR\syste <systemofadowng@gmail.com>
2024-05-10 21:46:21 -0400

    Set Matrix Zeroes

--- src/problems/SetMatrixZeroes.java
+++ src/problems/SetMatrixZeroes.java
@@ -2,26 +2,39 @@ package problems;
 
 public class SetMatrixZeroes {
     public void setZeroes(int[][] matrix) {
-
-        boolean[] rows = new boolean[matrix.length];
-        boolean[] columns = new boolean[matrix[0].length];
+        boolean firstRow = false;
+        boolean firstCol = false;
 
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j] == 0) {
-                    rows[i] = true;
-                    columns[j] = true;
+                    if(i == 0) {
+                        firstRow = true;
+                    } else if(j == 0) {
+                        firstCol = true;
+                    } else {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
                 }
             }
         }
-
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                if(rows[i] == true || columns[j] == true) {
-                    matrix[i][j] = 0;
-                }
+        for(int i = matrix.length - 1; i >= 0; i--) {
+            for(int j = matrix[0].length - 1; j >= 0; j--) {
+                if(matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
             }
         }
 
+        if(firstRow) {
+            for(int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(firstCol) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
