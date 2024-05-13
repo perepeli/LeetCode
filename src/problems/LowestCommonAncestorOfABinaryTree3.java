92f21399b0b9fd05c33203a2f9e7066747645b48
YMIR\syste <systemofadowng@gmail.com>
2024-05-13 00:35:01 -0400

    Lowest Common Ancestor of a Binary Tree III

---
+++ src/problems/LowestCommonAncestorOfABinaryTree3.java
@@ -0,0 +1,22 @@
+package problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LowestCommonAncestorOfABinaryTree3 {
+//    public Node lowestCommonAncestor(Node p, Node q) {
+//        Set<Node> path = new HashSet<>();
+//
+//        while(p != null) {
+//            path.add(p);
+//            p = p.parent;
+//        }
+//
+//        while(q != null) {
+//            if(path.contains(q)) return q;
+//            q = q.parent;
+//        }
+//
+//        return null;
+//    }
+}
