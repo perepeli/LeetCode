e2d88f5a98adcbfd1a5b36e06bc35399cc13806c
YMIR\syste <systemofadowng@gmail.com>
2024-05-23 22:36:41 -0400

    Bag of Tokens

---
+++ src/problems/BagOfTokens.java
@@ -0,0 +1,30 @@
+package problems;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BagOfTokens {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int score = 0;
+        Arrays.sort(tokens);
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int token : tokens) {
+            deque.add(token);
+        }
+
+        while (!deque.isEmpty()) {
+            if (power >= deque.peekFirst()) {
+                power -= deque.pollFirst();
+                score++;
+            } else if (deque.size() > 1 && score > 0) {
+                power += deque.pollLast();
+                score--;
+            } else {
+                return score;
+            }
+        }
+        return score;
+    }
+}
