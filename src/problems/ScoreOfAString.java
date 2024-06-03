e29784183c94bb93dbf4c8a373fc49a5e78f4ba4
YMIR\syste <systemofadowng@gmail.com>
2024-06-02 20:10:00 -0400

    Score of a String

---
+++ src/problems/ScoreOfAString.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class ScoreOfAString {
+    public int scoreOfString(String s) {
+        int res = 0;
+
+        for(int i = 1; i < s.length(); i++) {
+            res += Math.abs(s.charAt(i) - s.charAt(i - 1));
+        }
+
+        return res;
+    }
+}
