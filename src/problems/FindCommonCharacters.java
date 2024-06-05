f70842025b177b1454fbed3913d0a1c82c886001
YMIR\syste <systemofadowng@gmail.com>
2024-06-04 21:53:10 -0400

    Find Common Characters

--- src/problems/FindCommonCharacters.java
+++ src/problems/FindCommonCharacters.java
@@ -1,40 +1,30 @@
 package problems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindCommonCharacters {
-    public List<String> commonChars(String[] A) {
-        int[] last = count(A[0]);
-        for (int i = 1; i < A.length; i++) {
-            last = intersection(last, count(A[i]));
-        }
-        List<String> arr = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            if (last[i] != 0) {
-                char a = 'a';
-                a += i;
-                String s = String.valueOf(a);
-                while (last[i] > 0) {
-                    arr.add(s);
-                    last[i]--;
-                }
+    public List<String> commonChars(String[] words) {
+        int[] count = new int[26];
+        Arrays.fill(count, -1);
+
+        for(String s : words) {
+            int[] tempCount = new int[26];
+            for(int i = 0; i < s.length(); i++) tempCount[s.charAt(i) - 'a']++;
+            for(int i = 0; i < tempCount.length; i++){
+                if(count[i] == -1) count[i] = tempCount[i];
+                else count[i] = Math.min(count[i], tempCount[i]);
             }
         }
-        return arr;
-    }
 
-    int[] intersection(int[] a, int[] b) {
-        int[] t = new int[26];
-        for (int i = 0; i < 26; i++) {
-            t[i] = Math.min(a[i], b[i]);
+        List<String> res = new ArrayList<>();
+        for(int i = 0; i < count.length; i++) {
+            char c = (char)(i + 'a');
+            for(int j = 0; j < count[i]; j++) {
+                res.add(String.valueOf(c));
+            }
         }
-        return t;
-    }
-
-    int[] count(String str) {
-        int[] t = new int[26];
-        for (char c : str.toCharArray()) t[c - 'a']++;
-        return t;
+        return res;
     }
 }
