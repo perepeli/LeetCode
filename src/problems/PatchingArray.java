793f1cf40e172f06e7922dc33c7d76fdaf7e6efd
YMIR\syste <systemofadowng@gmail.com>
2024-06-16 16:39:33 -0400

    Patching Array

---
+++ src/problems/PatchingArray.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class PatchingArray {
+    public int minPatches(int[] nums, int n) {
+        int patches = 0, i = 0;
+        long miss = 1;
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss)
+                miss += nums[i++];
+            else {
+                miss += miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
