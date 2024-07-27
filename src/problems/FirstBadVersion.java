a84f237c2b8486c7996c6b39c0df7c70297afdc1
YMIR\syste <systemofadowng@gmail.com>
2024-07-27 03:23:54 -0400

    First Bad Version

--- src/problems/FirstBadVersion.java
+++ src/problems/FirstBadVersion.java
@@ -1,22 +1,19 @@
 package problems;
 
-//public class FirstBadVersion extends VersionControl{
+//public class Solution extends VersionControl {
 //    public int firstBadVersion(int n) {
-//        int left = 1;
+//        int left = 0;
 //        int right = n;
 //
-//        while(right-left > 1) {
-//            int middle = left+((right-left)/2);
-//            if(!isBadVersion(middle)) {
-//                left = middle;
+//        while(left < right) {
+//            int mid = left + (right - left) / 2;
+//            if(isBadVersion(mid)) {
+//                right = mid;
 //            } else {
-//                right = middle;
+//                left = mid + 1;
 //            }
 //        }
 //
-//        if(isBadVersion(left)) return left;
-//        if(isBadVersion(right)) return right;
-//
-//        return -1;
+//        return left;
 //    }
 //}
