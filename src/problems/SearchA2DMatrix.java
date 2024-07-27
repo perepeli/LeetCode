0d6629e9607c1ef6ec4eecfd1b81c143abc546a0
YMIR\syste <systemofadowng@gmail.com>
2024-07-27 01:50:04 -0400

    Search a 2D Matrix

---
+++ src/problems/SearchA2DMatrix.java
@@ -0,0 +1,29 @@
+package problems;
+
+public class SearchA2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int length = m * n;
+
+        int left = 0;
+        int right = length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if(matrix[row][col] == target) {
+                return true;
+            } else if(matrix[row][col] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
