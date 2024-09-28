63c7147af433a7cdc67e70b0c8ab127524087a9a
YMIR\syste <systemofadowng@gmail.com>
2024-09-28 16:31:22 -0400

    Minimum Common Value

---
+++ src/problems/MinimumCommonValue.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class MinimumCommonValue {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int left = 0;
+        int right = 0;
+
+        while(left < nums1.length && right < nums2.length) {
+            if(nums1[left] == nums2[right]) return nums1[left];
+            else if(nums1[left] < nums2[right]) left++;
+            else right++;
+        }
+
+        return -1;
+    }
+}
