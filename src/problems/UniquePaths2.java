35be6d184136d00087cbd25b5dbf9bdd6723e2d3
YMIR\syste <systemofadowng@gmail.com>
2024-11-22 20:24:53 -0500

    Unique Paths II

---
+++ src/problems/UniquePaths2.java
@@ -0,0 +1,29 @@
+package problems;
+
+public class UniquePaths2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+
+        for(int i = 0; i < obstacleGrid.length; i++) {
+            for(int j = 0; j < obstacleGrid[0].length; j++) {
+                if(obstacleGrid[i][j] != 1) {
+                    if(i == 0 || j == 0) {
+                        if(i == 0) {
+                            if(j > 0 && dp[i][j - 1] == 0) dp[i][j] = 0;
+                            else dp[i][j] = 1;
+                        }
+                        if(j == 0) {
+                            if(i > 0 && dp[i - 1][j] == 0) dp[i][j] = 0;
+                            else dp[i][j] = 1;
+                        }
+                    } else {
+                        dp[i][j] += dp[i - 1][j];
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
