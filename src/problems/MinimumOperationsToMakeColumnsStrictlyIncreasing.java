036c6503d0cd28adb08060bcab5ef6017264427b
YMIR\syste <systemofadowng@gmail.com>
2025-01-15 23:26:42 -0500

    Minimum Operations to Make Columns Strictly Increasing

--- src/problems/MinimumOperationsToMakeColumnsStrictlyIncreasing.java
+++ src/problems/MinimumOperationsToMakeColumnsStrictlyIncreasing.java
@@ -7,9 +7,9 @@ public class MinimumOperationsToMakeColumnsStrictlyIncreasing {
         for(int i = 1; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] <= grid[i - 1][j]) {
-                    int newNum = grid[i - 1][j] - grid[i][j] + 1;
-                    res += newNum;
-                    grid[i][j] += newNum;
+                    int diff = grid[i - 1][j] - grid[i][j] + 1;
+                    res += diff;
+                    grid[i][j] += diff;
                 }
             }
         }
