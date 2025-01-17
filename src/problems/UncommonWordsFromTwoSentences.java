6b18c5ec34cda7b1d94c1a24414af4059233f933
YMIR\syste <systemofadowng@gmail.com>
2025-01-16 22:13:12 -0500

    Uncommon Words from Two Sentences

--- src/problems/UncommonWordsFromTwoSentences.java
+++ src/problems/UncommonWordsFromTwoSentences.java
@@ -7,30 +7,23 @@ import java.util.Map;
 
 public class UncommonWordsFromTwoSentences {
     public String[] uncommonFromSentences(String s1, String s2) {
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for(String s : s1.split(" ")) {
-            map1.put(s, map1.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
         for(String s : s2.split(" ")) {
-            map2.put(s, map2.getOrDefault(s, 0) + 1);
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
 
         List<String> res = new ArrayList<>();
 
-        for(var entry : map1.entrySet()) {
-            if(!map2.containsKey(entry.getKey()) && entry.getValue() == 1) {
+        for(var entry : map.entrySet()) {
+            if(entry.getValue() == 1) {
                 res.add(entry.getKey());
             }
         }
 
-        for(var entry : map2.entrySet()) {
-            if(!map1.containsKey(entry.getKey()) && entry.getValue() == 1) {
-                res.add(entry.getKey());
-            }
-        }
-
-        return res.stream().toArray(String[]::new);
+        return res.toArray(String[]::new);
     }
 }
