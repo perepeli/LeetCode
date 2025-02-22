3a5dcb31eff436e14df077c7ffaa70a07dd14260
YMIR\syste <systemofadowng@gmail.com>
2025-02-22 00:16:26 -0500

    Flood Fill

--- src/problems/FloodFill.java
+++ src/problems/FloodFill.java
@@ -4,34 +4,20 @@ import java.util.Arrays;
 
 public class FloodFill {
 
-    public static void main(String[] args) {
-        int[][] arr1 = new int[][]{{1,1,1},{1,1,0}, {1,0,1}};
-        int[][] arr2 = new int[][]{{0,0,0},{0,0,0}};
-        floodFill(arr1, 1, 1, 2);
-
-        for (int i = 0; i < arr1.length; i++) {
-            System.out.println(Arrays.toString(arr1[i]));
-        }
-    }
-    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        recursive(image, sr, sc, image.length-1, image[0].length-1, image[sr][sc], color);
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length) return image;
+        if(image[sr][sc] == color) return image;
+        dfs(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 
-    private static void recursive(int[][] image, int sr, int sc, int rows, int columns, int target, int color) {
-        if(sr < 0 || sr > rows || sc < 0 || sc > columns) return;
-        if(image[sr][sc] == target) {
-            image[sr][sc] = color;
-        } else {
-            return;
-        }
-        //up
-        recursive(image, sr-1, sc, rows, columns, target, color);
-        //left
-        recursive(image, sr, sc-1, rows, columns, target, color);
-        //down
-        recursive(image, sr+1, sc, rows, columns, target, color);
-        //right
-        recursive(image, sr, sc+1, rows, columns, target, color);
+    private void dfs(int[][] image, int r, int c, int color, int originalColor) {
+        if(r < 0 || c < 0 || r >= image.length || c >= image[0].length) return;
+        if(image[r][c] != originalColor) return;
+        image[r][c] = color;
+        dfs(image, r + 1, c, color, originalColor);
+        dfs(image, r - 1, c, color, originalColor);
+        dfs(image, r, c + 1, color, originalColor);
+        dfs(image, r, c - 1, color, originalColor);
     }
 }
