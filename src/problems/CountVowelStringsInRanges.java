056dc0b59bfebc73d3852790198025b018b765fc
perepeli <systemofadowng@gmail.com>
2025-03-14 04:00:12 -0400

    Count Vowel Strings in Ranges

--- src/problems/CountVowelStringsInRanges.java
+++ src/problems/CountVowelStringsInRanges.java
@@ -1,44 +1,35 @@
 package problems;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class CountVowelStringsInRanges {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        Set<Character> vovSet = Set.of('a', 'e', 'i', 'o', 'u');
-        int[] prefix = new int[words.length];
-        int[] suffix = new int[words.length];
-        int count = 0;
-
-        for(String s : words) {
-            if(vovSet.contains(s.charAt(0)) && vovSet.contains(s.charAt(s.length() - 1))) {
-                count++;
+        int[] ans = new int[queries.length];
+        HashSet<Character> vowels = new HashSet<>(
+                Arrays.asList('a', 'e', 'i', 'o', 'u')
+        );
+        int[] prefixSum = new int[words.length];
+        int sum = 0;
+        for (int i = 0; i < words.length; i++) {
+            String currentWord = words[i];
+            if (
+                    vowels.contains(currentWord.charAt(0)) &&
+                            vowels.contains(currentWord.charAt(currentWord.length() - 1))
+            ) {
+                sum++;
             }
+            prefixSum[i] = sum;
         }
 
-        for(int i = 1; i < words.length; i++) {
-            prefix[i] = prefix[i - 1];
-            String prev = words[i - 1];
-            if(vovSet.contains(prev.charAt(0)) && vovSet.contains(prev.charAt(prev.length() - 1))) {
-                prefix[i]++;
-            }
+        for (int i = 0; i < queries.length; i++) {
+            int[] currentQuery = queries[i];
+            ans[i] =
+                    prefixSum[currentQuery[1]] -
+                            (currentQuery[0] == 0 ? 0 : prefixSum[currentQuery[0] - 1]);
         }
 
-        for(int i = words.length - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1];
-            String next = words[i + 1];
-            if(vovSet.contains(next.charAt(0)) && vovSet.contains(next.charAt(next.length() - 1))) {
-                suffix[i]++;
-            }
-        }
-
-        int[] res = new int[queries.length];
-        for(int i = 0; i < queries.length; i++) {
-            int[] query = queries[i];
-            int from = query[0];
-            int to = query[1];
-            res[i] = count - prefix[from] - suffix[to];
-        }
-
-        return res;
+        return ans;
     }
 }
