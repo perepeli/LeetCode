f66f406d338d8e1e098d8aaefd67e90413e5c877
perepeli <systemofadowng@gmail.com>
2025-03-27 03:14:43 -0400

    Daily Temperatures

--- src/problems/DailyTemperatures.java
+++ src/problems/DailyTemperatures.java
@@ -7,18 +7,13 @@ import java.util.Stack;
 
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<int[]> stack = new ArrayDeque<>();
         int[] res = new int[temperatures.length];
-
+        Deque<int[]> stack = new ArrayDeque<>();
         for(int i = temperatures.length - 1; i >= 0; i--) {
             int curr = temperatures[i];
-
-            while(!stack.isEmpty() && stack.peekLast()[0] <= curr) {
-                stack.removeLast();
-            }
-
-            res[i] = stack.isEmpty() ? 0 : stack.peekLast()[1] - i;
-            stack.addLast(new int[]{curr, i});
+            while(!stack.isEmpty() && stack.peekLast()[0] <= curr) stack.removeLast();
+            if(!stack.isEmpty()) res[i] = stack.peekLast()[1] - i;
+            stack.offer(new int[]{curr, i});
         }
 
         return res;
