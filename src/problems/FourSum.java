db920afec0601acfaba68fc39464b34fb099325d
perepeli <systemofadowng@gmail.com>
2025-05-14 19:45:40 -0400

    4Sum

--- src/problems/FourSum.java
+++ src/problems/FourSum.java
@@ -10,31 +10,16 @@ public class FourSum {
 
     public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
         List<List<Integer>> res = new ArrayList<>();
-
-        if (start == nums.length) {
-            return res;
-        }
+        if (start == nums.length) return res;
 
         long average_value = target / k;
 
-        if (
-                nums[start] > average_value || average_value > nums[nums.length - 1]
-        ) {
-            return res;
-        }
-
-        if (k == 2) {
-            return twoSum(nums, target, start);
-        }
+        if (nums[start] > average_value || average_value > nums[nums.length - 1]) return res;
+        if (k == 2) return twoSum(nums, target, start);
 
         for (int i = start; i < nums.length; ++i) {
             if (i == start || nums[i - 1] != nums[i]) {
-                for (List<Integer> subset : kSum(
-                        nums,
-                        target - nums[i],
-                        i + 1,
-                        k - 1
-                )) {
+                for (List<Integer> subset : kSum(nums, target - nums[i], i + 1, k - 1)) {
                     res.add(new ArrayList<>(Arrays.asList(nums[i])));
                     res.get(res.size() - 1).addAll(subset);
                 }
