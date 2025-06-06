9c92c207d2e5e588d05866482d2dbccc59c4f24f
perepeli <systemofadowng@gmail.com>
2025-06-06 16:21:30 -0400

    Sqrt(x)

---
+++ src/problems/SqrtX.java
@@ -0,0 +1,12 @@
+package problems;
+
+public class SqrtX {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = (int) Math.pow(Math.E, 0.5 * Math.log(x));
+        int right = left + 1;
+
+        return (long) right * right > x ? left : right;
+    }
+}
