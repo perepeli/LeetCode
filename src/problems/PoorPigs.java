f4b8422f18fbf9dad5538ab9d6296c430cd22e3d
perepeli <systemofadowng@gmail.com>
2025-06-23 18:11:55 -0400

    Poor Pigs

---
+++ src/problems/PoorPigs.java
@@ -0,0 +1,8 @@
+package problems;
+
+public class PoorPigs {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int states = minutesToTest / minutesToDie + 1;
+        return (int) Math.ceil(Math.log(buckets) / Math.log(states) - 1e-10);
+    }
+}
