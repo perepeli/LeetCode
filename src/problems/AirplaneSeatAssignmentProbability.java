2868ab0686fe2239c0243934bc7931509a87df32
perepeli <systemofadowng@gmail.com>
2025-06-30 16:44:02 -0400

    Airplane Seat Assignment Probability

---
+++ src/problems/AirplaneSeatAssignmentProbability.java
@@ -0,0 +1,8 @@
+package problems;
+
+public class AirplaneSeatAssignmentProbability {
+    public double nthPersonGetsNthSeat(int n) {
+        if(n==1) return 1;
+        else return 0.5;
+    }
+}
