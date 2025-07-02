aa7a5df47a02ed13a9e64cf39bc207e964c7bbe5
perepeli <systemofadowng@gmail.com>
2025-07-02 18:07:55 -0400

    Chalkboard XOR Game

---
+++ src/problems/ChalkboardXORGame.java
@@ -0,0 +1,9 @@
+package problems;
+
+public class ChalkboardXORGame {
+    public boolean xorGame(int[] nums) {
+        int x = 0;
+        for (int v : nums) x ^= v;
+        return x == 0 || nums.length % 2 == 0;
+    }
+}
