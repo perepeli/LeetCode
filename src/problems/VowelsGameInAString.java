df02bf38c248f1e5a7d8cdcdcae80e809dc40d02
perepeli <systemofadowng@gmail.com>
2025-07-07 19:21:43 -0400

    Vowels Game in a String

---
+++ src/problems/VowelsGameInAString.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class VowelsGameInAString {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
