71c98ac7664b3d66aef5f9357acba880479464fd
perepeli <systemofadowng@gmail.com>
2025-07-09 19:25:45 -0400

    Valid Sudoku

--- src/problems/ValidSudoku.java
+++ src/problems/ValidSudoku.java
@@ -7,29 +7,29 @@ import java.util.Set;
 
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
+        int[][] rows = new int[9][10];
+        int[][] cols = new int[9][10];
+        int[][] boxes = new int[9][10];
 
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                char c = board[i][j];
-                if(Character.isDigit(c)) {
-                    int index = (int)(c - '0' - 1);
+                int boxIndex = i / 3 * 3 + j / 3;
 
-                    if(cols[j][index]) return false;
-                    cols[j][index] = true;
+                if(board[i][j] != '.') {
+                    int num = Character.getNumericValue(board[i][j]);
 
-                    if(rows[i][index]) return false;
-                    rows[i][index] = true;
+                    if(rows[i][num] == 1) return false;
+                    rows[i][num]++;
 
-                    int boxIndex = 3 * (i / 3) + (j / 3);
+                    if(cols[j][num] == 1) return false;
+                    cols[j][num]++;
 
-                    if(boxes[boxIndex][index]) return false;
-                    boxes[boxIndex][index] = true;
+                    if(boxes[boxIndex][num] == 1) return false;
+                    boxes[boxIndex][num]++;
                 }
             }
         }
+
         return true;
     }
 }
