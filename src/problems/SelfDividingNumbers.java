3d309ddf8a3e2c29df51c6a421832a3a370ec4c5
perepeli <systemofadowng@gmail.com>
2025-07-30 00:21:42 -0400

    Self Dividing Numbers

---
+++ src/problems/SelfDividingNumbers.java
@@ -0,0 +1,25 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumbers {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList();
+        for (int n = left; n <= right; ++n) {
+            if (selfDividing(n)) {
+                ans.add(n);
+            }
+        }
+        return ans;
+    }
+
+    public boolean selfDividing(int n) {
+        for (char c: String.valueOf(n).toCharArray()) {
+            if (c == '0' || (n % (c - '0') > 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
