ce2813ec088fc5eaaa859ce9d80b6975c586ae15
perepeli <systemofadowng@gmail.com>
2025-08-13 14:37:18 -0400

    Minimum Cost Path with Alternating Directions I

---
+++ src/problems/MinimumCostPathWithAlternatingDirections1.java
@@ -0,0 +1,9 @@
+package problems;
+
+public class MinimumCostPathWithAlternatingDirections1 {
+    public int minCost(int m, int n) {
+        if (m == 1 && n == 1) return 1;
+        if (m == 2 && n == 1 || m == 1 && n == 2) return 3;
+        return -1;
+    }
+}
