2345a128f2681ec70fcb681e8365720ea08f51ab
perepeli <systemofadowng@gmail.com>
2025-08-28 17:39:37 -0400

    Missing Number In Arithmetic Progression

---
+++ src/problems/MissingNumberInArithmeticProgression.java
@@ -0,0 +1,16 @@
+package problems;
+
+public class MissingNumberInArithmeticProgression {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        int difference = (arr[arr.length - 1] - arr[0]) / n;
+        int expected = arr[0];
+
+        for (int val : arr) {
+            if (val != expected) return expected;
+            expected += difference;
+        }
+
+        return expected;
+    }
+}
