6db6b174404729c49fb3d013f63d44dafe752d72
perepeli <systemofadowng@gmail.com>
2025-09-05 09:45:52 -0400

    XOR Operation in an Array

---
+++ src/problems/XOROperationInAnArray.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class XOROperationInAnArray {
+    public int xorOperation(int n, int start) {
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            result ^= (start + 2 * i);
+        }
+
+        return result;
+    }
+}
