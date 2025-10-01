be599096b2b012fe6d5027d45166e58af0e5d1e2
perepeli <systemofadowng@gmail.com>
2025-10-01 12:00:11 -0400

    Construct the Rectangle

---
+++ src/problems/ConstructTheRectangle.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class ConstructTheRectangle {
+    public int[] constructRectangle(int area) {
+        int w = (int)Math.sqrt(area);
+
+        while( area % w != 0){
+            w--;
+        }
+
+        return new int[]{area/w, w};
+    }
+}
