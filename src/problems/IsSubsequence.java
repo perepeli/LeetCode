ce7e0e1d31460ae4ca505d4acfd4cc01fdde5ccd
perepeli <systemofadowng@gmail.com>
2025-10-12 09:53:06 -0400

    Is Subsequence

--- src/problems/IsSubsequence.java
+++ src/problems/IsSubsequence.java
@@ -2,22 +2,12 @@ package problems;
 
 public class IsSubsequence {
     public boolean isSubsequence(String s, String t) {
+        int index = 0;
 
-        int index = 0; //0
-        int length = s.length() -1; //2
-
-        for(int i = 0; i < t.length(); i++) {
-            if(index == length+1) return true; //==3?
-
-            char cs = s.charAt(index);
-            char ct = t.charAt(i);
-            boolean b = cs == ct;
-            if(t.charAt(i) == s.charAt(index)) {
-                index++;
-            }
+        for(int i = 0; i < t.length() && index < s.length(); i++) {
+            if(t.charAt(i) == s.charAt(index)) index++;
         }
-        if(index == length+1) return true;
 
-        return false;
+        return index == s.length();
     }
 }
