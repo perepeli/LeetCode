83a583dcbe62a817dcd6fbf579ce3b0b6a7f0f59
perepeli <systemofadowng@gmail.com>
2025-10-13 21:57:38 -0400

    Armstrong Number

---
+++ src/problems/ArmstrongNumber.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class ArmstrongNumber {
+    public int getSumOfKthPowerOfDigits(int n, int k) {
+        int result = 0;
+
+        while (n != 0) {
+            result += Math.pow(n % 10, k);
+            n /= 10;
+        }
+
+        return result;
+    }
+    public boolean isArmstrong(int n) {
+        int length = String.valueOf(n).length();
+        return getSumOfKthPowerOfDigits(n, length) == n;
+    }
+}
