b37c53e8873f5ede8be394a9f45ad7a73182b4cd
perepeli <systemofadowng@gmail.com>
2025-10-22 10:28:55 -0400

    Reaching Points

---
+++ src/problems/ReachingPoints.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class ReachingPoints {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx >= sx && ty >= sy){
+            if(tx > ty) {
+                if(sy == ty) return (tx - sx) % ty == 0;
+                tx %= ty;
+            }
+            else {
+                if(sx == tx) return (ty - sy) % tx == 0;
+                ty %= tx;
+            }
+        }
+        return false;
+    }
+}
