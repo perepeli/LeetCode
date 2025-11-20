bed7463aebd83f8b5413e4b386fee63ba6c823b8
perepeli <systemofadowng@gmail.com>
2025-11-20 14:24:45 -0500

    Minimum Moves to Equal Array Elements II

---
+++ src/problems/MinimumMovesToEqualArrayElements2.java
@@ -0,0 +1,15 @@
+package problems;
+
+public class MinimumMovesToEqualArrayElements2 {
+    public int minMoves2(int[] nums) {
+        long min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            long sum = 0;
+            for (int n : nums) {
+                sum += Math.abs(n - num);
+            }
+            min = Math.min(min, sum);
+        }
+        return (int) min;
+    }
+}
