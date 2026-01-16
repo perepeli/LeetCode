50342ad319ff354e9a20a46ef9e6a9b4875c33c9
perepeli <systemofadowng@gmail.com>
2026-01-16 18:51:18 -0500

    Shuffle an Array

---
+++ src/problems/ShuffleAnArray.java
@@ -0,0 +1,43 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ShuffleAnArray {
+    private int[] array;
+    private int[] original;
+
+    private Random rand = new Random();
+
+    private List<Integer> getArrayCopy() {
+        List<Integer> asList = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            asList.add(array[i]);
+        }
+        return asList;
+    }
+
+    public ShuffleAnArray(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return array;
+    }
+
+    public int[] shuffle() {
+        List<Integer> aux = getArrayCopy();
+
+        for (int i = 0; i < array.length; i++) {
+            int removeIdx = rand.nextInt(aux.size());
+            array[i] = aux.get(removeIdx);
+            aux.remove(removeIdx);
+        }
+
+        return array;
+    }
+}
