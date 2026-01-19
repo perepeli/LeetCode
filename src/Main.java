5cf581b0460453293a942eff721395a4ef438117
perepeli <systemofadowng@gmail.com>
2026-01-19 13:19:44 -0500

    Alert Using Same Key-Card Three or More Times in a One Hour Period

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Singleton singleton = Singleton.getInstance();
+        Map<String, Set<String>> graph = Map.of(
+                "A", Set.of("B", "C"),
+                "B", Set.of("C"),
+                "C", Set.of("D")
+        );
 
-        System.out.println('a' + 1);
+        Map<String, Integer> indegree = new HashMap<>();
 
-    }
-}
+        for(var e : graph.entrySet()) {
+            for(String child : e.getValue()) {
+                indegree.put(child, indegree.getOrDefault(child, 0) + 1);
+            }
+        }
+
+        indegree.put("A", 1);
+
+        Map<String, Integer> load = new HashMap<>();
+
+        load.put("A", 1);
+
+        for(String parent : indegree.keySet()) {
+            int parentLoad = load.get(parent);
+            if(!graph.containsKey(parent)) continue;
+            for(String child : graph.get(parent)) {
+                load.put(child, load.getOrDefault(child, 0) + parentLoad);
+            }
+        }
 
 
-class Singleton {
 
-    private Singleton() {}
 
-    private static class SingletonHelper {
-        private static final Singleton uniqueInstance = new Singleton();
-    }
 
-    public static Singleton getInstance() {
-        return SingletonHelper.uniqueInstance;
+        System.out.println(indegree);
+        System.out.println(load);
+
     }
 }
 
