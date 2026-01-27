2c0bfad970146eadcc88a9da6ebe78aa2d50e59e
perepeli <systemofadowng@gmail.com>
2026-01-27 13:59:33 -0500

    Implement Rand10() Using Rand7()

---
+++ src/problems/ImplementRand10UsingRand7.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class ImplementRand10UsingRand7 {
+//    public int rand10() {
+//        int row, col, idx;
+//        do {
+//            row = rand7();
+//            col = rand7();
+//            idx = col + (row - 1) * 7;
+//        } while (idx > 40);
+//        return 1 + (idx - 1) % 10;
+//    }
+}
