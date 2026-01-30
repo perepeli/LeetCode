7af085824b78a47e6420336a4afc405b261a9adf
perepeli <systemofadowng@gmail.com>
2026-01-30 12:40:37 -0500

    Plus One Linked List

---
+++ src/problems/PlusOneLinkedList.java
@@ -0,0 +1,28 @@
+package problems;
+
+import problems.util.ListNode;
+
+public class PlusOneLinkedList {
+    public ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode notNine = dummy;
+
+        while (head != null) {
+            if (head.val != 9) {
+                notNine = head;
+            }
+            head = head.next;
+        }
+
+        notNine.val++;
+        notNine = notNine.next;
+
+        while (notNine != null) {
+            notNine.val = 0;
+            notNine = notNine.next;
+        }
+
+        return dummy.val != 0 ? dummy : dummy.next;
+    }
+}
