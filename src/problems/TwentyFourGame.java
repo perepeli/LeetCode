10637ea72bd4c480658f1de7547993d30e448603
perepeli <systemofadowng@gmail.com>
2026-02-09 13:58:05 -0500

    24 Game

---
+++ src/problems/TwentyFourGame.java
@@ -0,0 +1,58 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwentyFourGame {
+    private List<Double> generatePossibleResults(double a, double b) {
+        List<Double> res = new ArrayList<>();
+        res.add(a + b);
+        res.add(a - b);
+        res.add(b - a);
+        res.add(a * b);
+        if (b != 0) {
+            res.add(a / b);
+        }
+        if (a != 0) {
+            res.add(b / a);
+        }
+        return res;
+    }
+
+    private boolean checkIfResultReached(List<Double> list) {
+        if (list.size() == 1) {
+            return Math.abs(list.get(0) - 24) <= 0.1;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                List<Double> newList = new ArrayList<>();
+                for (int k = 0; k < list.size(); k++) {
+                    if (k != j && k != i) {
+                        newList.add(list.get(k));
+                    }
+                }
+
+                for (double res : generatePossibleResults(list.get(i), list.get(j))) {
+                    newList.add(res);
+
+                    if (checkIfResultReached(newList)) {
+                        return true;
+                    }
+
+                    newList.remove(newList.size() - 1);
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean judgePoint24(int[] cards) {
+        List<Double> list = new ArrayList<>();
+        for (int card : cards) {
+            list.add((double) card);
+        }
+
+        return checkIfResultReached(list);
+    }
+}
