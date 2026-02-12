e65f88f581f50400a81259d501b20d8ea01c47bd
perepeli <systemofadowng@gmail.com>
2026-02-12 16:22:03 -0500

    Rabbits in Forest

---
+++ src/problems/RabbitsInForest.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class RabbitsInForest {
+    public int numRabbits(int[] answers) {
+        int[] count = new int[1000];
+        for (int x: answers) count[x]++;
+
+        int ans = 0;
+        for (int k = 0; k < 1000; ++k)
+            ans += Math.floorMod(-count[k], k+1) + count[k];
+        return ans;
+    }
+}
