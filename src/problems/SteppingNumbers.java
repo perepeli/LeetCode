8533a0ebbe06b7c58c6416cd2ef62519c21dbc6d
perepeli <systemofadowng@gmail.com>
2026-04-05 12:46:51 -0400

    Stepping Numbers

---
+++ src/problems/SteppingNumbers.java
@@ -0,0 +1,34 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class SteppingNumbers {
+    public List<Integer> countSteppingNumbers(int low, int high) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        if(low==0) result.add(0);
+        for(int i=1;i<=9;i++)
+            queue.add(i);
+
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            if(curr>=low&&curr<=high)
+                result.add(curr);
+            if(curr>high/10) continue;
+            int lastDigit = curr%10;
+
+            if(lastDigit!=9&&curr*10+lastDigit+1<=high)
+                queue.offer(curr*10+lastDigit+1);
+
+            if(lastDigit!=0&&curr*10+lastDigit-1<=high)
+                queue.offer(curr*10+lastDigit-1);
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+}
