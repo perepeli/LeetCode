ef1c09627492f5e9f9de47daceeffb3178187c13
perepeli <systemofadowng@gmail.com>
2026-04-22 21:03:51 -0400

    Convert to Base -2

---
+++ src/problems/ConvertToBase2.java
@@ -0,0 +1,12 @@
+package problems;
+
+public class ConvertToBase2 {
+    public String baseNeg2(int n) {
+        StringBuilder res = new StringBuilder();
+        while(n != 0){
+            res.append(n & 1);
+            n = -(n >> 1);
+        }
+        return (res.length() > 0) ? res.reverse().toString() : "0";
+    }
+}
