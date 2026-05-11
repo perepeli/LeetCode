45cf332b2f3894901958fbc0772c080434a17869
perepeli <systemofadowng@gmail.com>
2026-05-10 20:28:41 -0700

    Count Number of Nice Subarrays

---
+++ src/problems/CountNumberOfNiceSubarrays.java
@@ -0,0 +1,24 @@
+package problems;
+
+public class CountNumberOfNiceSubarrays {
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private int atMost(int[] nums, int k) {
+        int windowSize = 0, subarrays = 0, start = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            windowSize += nums[end] % 2;
+
+            while (windowSize > k) {
+                windowSize -= nums[start] % 2;
+                start++;
+            }
+
+            subarrays += end - start + 1;
+        }
+
+        return subarrays;
+    }
+}
