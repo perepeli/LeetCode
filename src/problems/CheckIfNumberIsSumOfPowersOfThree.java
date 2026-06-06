f0e351d284a2cdd2371dad65fe3c20cf3d1ef67c
perepeli <systemofadowng@gmail.com>
2026-06-06 09:58:50 -0700

    Check if Number is a Sum of Powers of Three

---
+++ src/problems/CheckIfNumberIsSumOfPowersOfThree.java
@@ -0,0 +1,15 @@
+package problems;
+
+public class CheckIfNumberIsSumOfPowersOfThree {
+    public boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+
+            n /= 3;
+        }
+
+        return true;
+    }
+}
