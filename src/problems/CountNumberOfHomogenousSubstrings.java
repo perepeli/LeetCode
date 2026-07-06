608122fdc073add4a7c61c839a03e6bdcc892347
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-07-06 09:22:26 -0700

    Count Number of Homogenous Substrings

---
+++ src/problems/CountNumberOfHomogenousSubstrings.java
@@ -0,0 +1,21 @@
+package problems;
+
+public class CountNumberOfHomogenousSubstrings {
+    public int countHomogenous(String s) {
+        int ans = 0;
+        int currStreak = 0;
+        int MOD = (int) 1e9 + 7;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || s.charAt(i) == s.charAt(i - 1)) {
+                currStreak++;
+            } else {
+                currStreak = 1;
+            }
+
+            ans = (ans + currStreak) % MOD;
+        }
+
+        return ans;
+    }
+}
