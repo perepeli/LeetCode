132b7e9a2d5b43f15b4835ab1edfadd63d4ba9f7
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-07-19 09:39:15 -0700

    Count Subarrays With Majority Element I

---
+++ src/problems/CountSubarraysWithMajorityElement1.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class CountSubarraysWithMajorityElement1 {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int cnt = 0;
+            for (int j = i; j < n; ++j) {
+                cnt += (nums[j] == target ? 1 : -1);
+                if (cnt > 0) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
