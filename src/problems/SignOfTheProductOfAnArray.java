4f095f5c544be2f11eff43cd5f77686d4cd614e2
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-07-23 13:32:47 -0700

    Sign of the Product of an Array

---
+++ src/problems/SignOfTheProductOfAnArray.java
@@ -0,0 +1,14 @@
+package problems;
+
+public class SignOfTheProductOfAnArray {
+    public int arraySign(int[] nums) {
+        var sign = 1;
+
+        for (var n : nums) {
+            if (n == 0) return 0;
+            sign *= n > 0 ? 1 : -1;
+        }
+
+        return sign;
+    }
+}
