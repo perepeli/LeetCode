57ed822685e46619e161f7559472f33794188a9a
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-08-11 23:10:43 -0700

    Kth Distinct String in an Array

---
+++ src/problems/KthDistinctStringInAnArray.java
@@ -0,0 +1,19 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthDistinctStringInAnArray {
+    public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for(String s : arr) map.put(s, map.getOrDefault(s, 0) + 1);
+
+        for(String s : arr) {
+            if(map.get(s) == 1 && --k == 0) {
+                return s;
+            }
+        }
+
+        return "";
+    }
+}
