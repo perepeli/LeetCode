3c149729c13b213bf47a40a772acefb7a9fb4693
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-08-13 09:54:41 -0700

    Maximum Value of a String in an Array

---
+++ src/problems/MaximumValueOfAStringInAnArray.java
@@ -0,0 +1,25 @@
+package problems;
+
+public class MaximumValueOfAStringInAnArray {
+    public int maximumValue(String[] strs) {
+        int n = strs.length, maxValue = 0;
+
+        for(String s : strs){
+            if(isInteger(s)){
+                maxValue = Math.max(maxValue, Integer.parseInt(s));
+            }
+            else{
+                maxValue = Math.max(maxValue, s.length());
+            }
+        }
+        return maxValue;
+    }
+    private boolean isInteger(String s) {
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
