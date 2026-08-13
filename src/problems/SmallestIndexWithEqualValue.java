172b08203ca08dc3099e5d55a1398d528e148ec3
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-08-12 21:28:06 -0700

    Smallest Index With Equal Value

---
+++ src/problems/SmallestIndexWithEqualValue.java
@@ -0,0 +1,11 @@
+package problems;
+
+public class SmallestIndexWithEqualValue {
+    public int smallestEqual(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            if(i % 10 == nums[i]) return i;
+        }
+
+        return -1;
+    }
+}
