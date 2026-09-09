268d69a2924bab43f204288f79bd6a747b899022
Danylo Perepelitsyn <danylo.perepelitsyn@gmail.com>
2026-09-09 16:45:34 -0700

    Find the Degree of Each Vertex

---
+++ src/problems/FindTheDegreeOfEachVertex.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class FindTheDegreeOfEachVertex {
+    public int[] findDegrees(int[][] matrix) {
+        int[] res = new int[matrix.length];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 1) {
+                    res[j]++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
